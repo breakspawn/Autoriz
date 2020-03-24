4b8decd04854012912955c73aa970b4a318bb5fa
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-03-24 18:07:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/autoriz/Empty.java
@@ -0,0 +1,45 @@
+package com.example.autoriz;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Empty extends AppCompatActivity {
+
+   private ListView list;
+   private String[] names = new String[] {"John", "Alex", "Max", "Bob"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+
+       list = (ListView) findViewById(R.id.activ);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.activ, names);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View v, int i, long l) {
+                        String val = Long.toString(list.getItemIdAtPosition(i));
+                        Toast.makeText(Empty.this, "Позиция" + i + ", значение" + val,
+                                Toast.LENGTH_LONG
+                        ).show();
+
+                    }
+                }
+        );
+    }
+
+}
---
+++ app/src/main/java/com/example/autoriz/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.autoriz;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
+    private EditText email;
+    private EditText pass;
+    private Button enter;
+    private Button registr;
+
+
+    private static final String email_txt = "breakspawn1@gmail.com";
+    private static final String pass_txt  = "1";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login();
+
+
+
+    }
+
+
+
+
+
+    public void login () {
+    email = (EditText)findViewById(R.id.Email);
+    pass = (EditText)findViewById(R.id.Password);
+    enter = (Button)findViewById(R.id.Enter);
+    registr = (Button)findViewById(R.id.Registr);
+    img = (ImageView)findViewById(R.id.Back);
+
+
+
+                        enter.setOnClickListener(
+                                new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        if (email_txt.equals(String.valueOf(email.getText())) && pass_txt.equals(String.valueOf(pass.getText()))) {
+                                            Intent intent = new Intent("com.example.autoriz.Empty");
+                                        startActivity(intent);
+                                            Toast.makeText(MainActivity.this, "Верный пароль",
+                                                    Toast.LENGTH_LONG
+                                            ).show();
+                                        }
+
+                                         else {
+                                            Toast.makeText(MainActivity.this, "Неверный пароль",
+                                                    Toast.LENGTH_LONG
+                                            ).show();
+                                        }
+                                    }
+                               }
+                        );
+                        registr.setOnClickListener(
+                                new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Toast.makeText(MainActivity.this, "Успешная регистрация",
+                                                Toast.LENGTH_LONG
+                                        ).show();
+                                    }
+
+                                }
+                        );
+
+                    }
+
+
+    }
+
+
+
