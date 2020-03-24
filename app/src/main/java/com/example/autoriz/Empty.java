8735e74112a77ad52b4917893dfcf0f6019016e0
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-03-24 20:54:37 +0300

    'исправлен'

--- app/src/main/java/com/example/autoriz/Empty.java
+++ app/src/main/java/com/example/autoriz/Empty.java
@@ -1,3 +1,4 @@
+
 package com.example.autoriz;
 
 
@@ -12,31 +13,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Empty extends AppCompatActivity {
 
-   private ListView list;
-   private String[] names = new String[] {"John", "Alex", "Max", "Bob"};
+    private ListView list;
+    private String[] names = new String[] {"John", "Alex", "Max", "Bob"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_empty);
 
         list = (ListView) findViewById(R.id.activ);
         ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.support_simple_spinner_dropdown_item, names);
         list.setAdapter(adapter);
 
-
         list.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View v, int i, long l) {
-
-
-
                         String id = Long.toString(list.getItemIdAtPosition(i));
                         String name = (String) list.getItemAtPosition(i);
 
                         Toast.makeText(Empty.this, "В списке: " + Integer.toString(i+1) + "\n ID элемента: " + id + "\n Имя: " + name,
-
                                 Toast.LENGTH_LONG
                         ).show();
                     }
