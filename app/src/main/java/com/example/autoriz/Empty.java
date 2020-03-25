cf51117c905a7d3bb0b83b710759fd6b89b442ed
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-03-25 17:22:23 +0300

    'изменение_цвета_текста'

--- app/src/main/java/com/example/autoriz/Empty.java
+++ app/src/main/java/com/example/autoriz/Empty.java
@@ -13,9 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Empty extends AppCompatActivity {
 
-    private SeekBar seek;
-    private ListView list;
-    private String[] names = new String[] {"John", "Alex", "Max", "Bob"};
+    private SeekBar seek; //ползунок
+    private ListView list; //текст
+    private String[] names = new String[] {"John", "Alex", "Max", "Bob"}; //массив имен для текста
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +23,20 @@ public class Empty extends AppCompatActivity {
         setContentView(R.layout.activity_empty);
 
         list = (ListView) findViewById(R.id.activ);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.support_simple_spinner_dropdown_item, names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.activ, names); //назночение массива для тектса
         list.setAdapter(adapter);
-        seek = (SeekBar)findViewById(R.id.seekBar);
+        seek = (SeekBar)findViewById(R.id.seekBar); //ползунок переменная seek
 
         list.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
-                    public void onItemClick(AdapterView<?> parent, View v, int i, long l) {
-                        String id = Long.toString(list.getItemIdAtPosition(i));
-                        String name = (String) list.getItemAtPosition(i);
+                    public void onItemClick(AdapterView<?> parent, View v, int i, long l) { //считывание кнопок
+                        String id = Long.toString(list.getItemIdAtPosition(i)); //превод символов в строку
+                        String name = (String) list.getItemAtPosition(i); //позиции для строк
 
+                        //вывод в тост
                         Toast.makeText(Empty.this, "В списке: " + Integer.toString(i+1) + "\n ID элемента: " + id + "\n Имя: " + name
-                                        + ", прогресс" + seek.getProgress(),
+                                        + ", прогресс" + seek.getProgress(), //считывание положения ползунка SeekBar
                                 Toast.LENGTH_LONG
                         ).show();
                     }
